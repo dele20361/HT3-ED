d0a46e15263816eb6fc7c05fabf6bd50511ece9a
dele20361 <64100118+dele20361@users.noreply.github.com>
2021-02-15 22:17:03 -0600

    Added Merge sort

--- Sorts.java
+++ Sorts.java
@@ -45,4 +45,83 @@ public class Sorts {
         }
     }
     
+    
+    /**
+     * Method to sort numbers wiht Merge Sort.
+     * @param List ArrayList that contains numbers.
+     * @return ArrayList sorted.
+     * @author Paola De León
+     */
+    public ArrayList<Integer> MergeSort( ArrayList<Integer> List ) {
+        
+        // Check if the size of the ArrayList is equal to 1.
+        if ( List.size() > 1 ) {
+
+            int middle = (List.size())/2;
+            // Create an ArrayList for the left side
+            ArrayList<Integer> LeftSide = new ArrayList<Integer>();
+            for (int i = 0; i < middle; i++) {
+                LeftSide.add ( List.get(i) );
+            }
+            // Create an ArrayList for the right side
+            ArrayList<Integer> RightSide = new ArrayList<Integer>();
+            for (int i = middle; i < List.size(); i++) {
+                RightSide.add( List.get(i) );
+            }  
+
+            ArrayList<Integer> Final = new ArrayList<Integer>();
+
+            // Recusive call
+            LeftSide = MergeSort(LeftSide);
+            RightSide = MergeSort(RightSide);
+
+            Final = Merge(LeftSide, RightSide);
+            return Final;
+
+        } else {
+            return List;
+        }
+
+    }
+
+
+    /**
+     * Second method to sort numbers.
+     * @param LeftSide Left side list.
+     * @param RightSide Right side list.
+     * @return ArrayList with numbers sorted.
+     */
+    private ArrayList<Integer> Merge( ArrayList<Integer> LeftSide, ArrayList<Integer> RightSide ) {
+        
+        ArrayList<Integer> Final = new ArrayList<Integer>();
+        int LeftSideIndx = 0;
+        int RightSideIndx = 0;
+
+        while ( ( LeftSideIndx < LeftSide.size() ) || ( ( RightSideIndx < RightSide.size() ) ) ) {
+            // Check if the left and right side have elements
+            if ( ( LeftSideIndx < LeftSide.size() ) && ( RightSideIndx < RightSide.size() ) ) {
+                // Compare to know witch number is greater.
+                if ( LeftSide.get( LeftSideIndx ) <= RightSide.get( RightSideIndx ) ) {
+                     Final.add( LeftSide.get( LeftSideIndx ) );
+                     LeftSideIndx ++;
+                } else {
+                    Final.add( RightSide.get( RightSideIndx ) );
+                    RightSideIndx ++;
+                }
+            } else if ( LeftSideIndx < LeftSide.size() ) {
+                // If right arraylist doesn't have elements
+                Final.add( LeftSide.get( LeftSideIndx ) );
+                LeftSideIndx ++;
+            } else if ( RightSideIndx < RightSide.size() ) {
+                // If left arraylist doesn't have elements
+                Final.add( RightSide.get( RightSideIndx ) );
+                RightSideIndx ++;
+            }
+
+        }
+
+        return Final;
+    }
+
+    
 }
