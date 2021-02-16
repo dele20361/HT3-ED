7655fc28e4e1175f544527dafe45eb979e947c99
dele20361 <64100118+dele20361@users.noreply.github.com>
2021-02-15 18:53:55 -0600

    Implementación de opción 1

--- Driver.java
+++ Driver.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 /**
  * Driver class of the program
@@ -13,14 +14,17 @@ public class Driver {
 
         // Properties
         Vista v = new Vista();
+        Sorts s = new Sorts();
 
         // Request number
         int num = v.InputInt();
+        Random rand = new Random();
 
         // Create ArrayList
         ArrayList<Integer> Numbers = new ArrayList<Integer>();
         for (int i = 0; i < num; i++) {
-            Numbers.add(i, i);
+            int random = rand.nextInt(num);
+            Numbers.add(i, random);
         }
 
         // Create file
@@ -33,41 +37,46 @@ public class Driver {
     
         // Welcome
         v.Welcome();
+        int opc = 0;
+        while (opc !=6) {
+            // Menu
+            opc = v.Menu();
+            switch (opc) {
+                case 1:
+                    // Gnome sort
+                    v.PrintList(Numbers);
+                    s.GnomeSort(Numbers);
+                    v.PrintList(Numbers);
+                    break;
+            
+                case 2:
+                    // Merge sort
 
-        // Menu
-        int opc = v.Menu();
-        switch (opc) {
-            case 1:
-                // Gnome sort
-                
-                break;
-        
-            case 2:
-                // Merge sort
+                    break;
+            
+                case 3:
+                    // Quick sort
 
-                break;
-        
-            case 3:
-                // Quick sort
+                    break;
+            
+                case 4:
+                    // Radix sort
 
-                break;
-        
-            case 4:
-                // Radix sort
+                    break;
+            
+                case 5:
+                    // Bubble sort
+                    
+                    break;
+            
+                case 6:
+                    v.Exit();
+                    break;
 
-                break;
-        
-            case 5:
-                // Bubble sort
-                
-                break;
-        
-            case 6:
-                v.Exit();
-                break;
-
-            default:
-                break;
+                default:
+                    v.Error();
+                    break;
+            }
         }
 
     }
